1e6e32f3d5862322fff2a67cad5f08bf4d194150
pieunseo <pieunseo@DESKTOP-3DU4RUM>
2025-03-12 09:15:05 +0900

    초기 커밋.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project02_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //계산시 프로그램 제작
+
+        }
+    }
+}
